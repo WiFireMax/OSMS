75bc8847a658abba170f04463ce194f42e43713b
WiFireMax <maxbakharev.nsk@gmail.com>
2023-12-06 19:50:02 +0700

    LAB05

--- LAB05/LAB05.c
+++ LAB05/LAB05.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define PACKET_LENGTH 23 //250
+#define PACKET_LENGTH 250 //23
 #define CRC_LENGTH 7
 
 int main() {
@@ -49,13 +49,16 @@ int main() {
         int bit = packet[i];
         packet[i] = !bit; // инвертирование бита
         int remainder = 0;
-        for (j = 0; j < PACKET_LENGTH + CRC_LENGTH; j++) {
-            if (remainder == 0 && j < PACKET_LENGTH) {
-                continue;
+        for (j = 0; j < PACKET_LENGTH; j++) {
+            if (packet[j] == 1) {
+            for (int k = 0; k < CRC_LENGTH; k++) {
+                packet[j+k] = packet[j+k] ^ generator[k];
             }
-            remainder = (remainder << 1) | packet[j];
-            if (remainder & (1 << CRC_LENGTH)) {
-                remainder ^= (generator[0] << CRC_LENGTH);
+            }
+        }
+        for (int h = PACKET_LENGTH; h < PACKET_LENGTH + CRC_LENGTH; h++) {
+            if (packet[h] == 1) {
+                remainder += 1;
             }
         }
         if (remainder == 0) {
@@ -66,36 +69,7 @@ int main() {
         }
         packet[i] = bit; // восстановление исходного бита
     }
-/*
-int previous_bits[PACKET_LENGTH + CRC_LENGTH];
-    int errors_detected = 0;
-    int errors_undetected = 0;
-    for (i = 0; i < PACKET_LENGTH + CRC_LENGTH; i++) {
-        int bit = packet[i];
-        packet[i] = !bit; // инвертирование бита
-        int remainder = 0;
-        for (j = 0; j < PACKET_LENGTH + CRC_LENGTH; j++) {
-            if (remainder == 0 && j < PACKET_LENGTH) {
-                continue;
-            }
-            remainder = (remainder << 1) | packet[j];
-            if (remainder & (1 << CRC_LENGTH)) {
-                remainder ^= (generator[0] << CRC_LENGTH);
-            }
-        }
-        if (remainder == 0) {
-            errors_undetected++;
-        } else {
-            errors_detected++;
-            for (j = 0; j < PACKET_LENGTH + CRC_LENGTH; j++) {
-                previous_bits[j] = packet[j];
-            }
-            for (j = 0; j < PACKET_LENGTH + CRC_LENGTH; j++) {
-                packet[j] = previous_bits[j];
-            }
-        }
-    }
-*/
+
     // Вывод отчета об ошибках
     printf("Количество обнаруженных ошибок: %d\n", errors_detected);
     printf("Количество необнаруженных ошибок: %d\n", errors_undetected);
