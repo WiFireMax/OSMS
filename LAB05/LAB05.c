aba095c2c9c56f8639b357e9d3e15a5f6dfced14
WiFireMax <maxbakharev.nsk@gmail.com>
2023-12-07 11:16:46 +0700

    LAB05 Final

--- LAB05/LAB05.c
+++ LAB05/LAB05.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define PACKET_LENGTH 250 //23
-#define CRC_LENGTH 7
+#define PACKET_LENGTH 800
+#define CRC_LENGTH 3
 
 int main() {
     int packet[PACKET_LENGTH + CRC_LENGTH];
+    int call_packet[PACKET_LENGTH + CRC_LENGTH];
     int generator[] = {1, 1, 1, 0, 1, 1, 1, 1}; // порождающий полином G для делителя: 11101111
     int i, j;
 	srand(time(NULL));
     // Создание рандомного пакета данных
     for (i = 0; i < PACKET_LENGTH; i++) {
         packet[i] = rand() % 2;
+        call_packet[i] = packet[i];
     }
 
     // Вывод рандомного пакета данных
@@ -29,7 +31,7 @@ int main() {
     // Вычисление CRC
     for (i = 0; i < PACKET_LENGTH; i++) {
         if (packet[i] == 1) {
-            for (j = 0; j < CRC_LENGTH; j++) {
+            for (j = 0; j < CRC_LENGTH+1; j++) {
                 packet[i+j] = packet[i+j] ^ generator[j];
             }
         }
@@ -42,22 +44,27 @@ int main() {
     }
     printf("\n");
 
+    for (int y = 0; y < PACKET_LENGTH + CRC_LENGTH; y++) {
+        call_packet[y] = call_packet[y] + packet[y];
+        }
+
     // Искажение пакета данных и CRC
     int errors_detected = 0;
     int errors_undetected = 0;
     for (i = 0; i < PACKET_LENGTH + CRC_LENGTH; i++) {
-        int bit = packet[i];
-        packet[i] = !bit; // инвертирование бита
+        int bit = call_packet[i];
+        call_packet[i] = !bit; // инвертирование бита
         int remainder = 0;
         for (j = 0; j < PACKET_LENGTH; j++) {
-            if (packet[j] == 1) {
-            for (int k = 0; k < CRC_LENGTH; k++) {
-                packet[j+k] = packet[j+k] ^ generator[k];
+            if (call_packet[j] == 1) {
+            for (int k = 0; k < CRC_LENGTH + 1; k++) {
+                call_packet[j+k] = call_packet[j+k] ^ generator[k];
             }
             }
         }
-        for (int h = PACKET_LENGTH; h < PACKET_LENGTH + CRC_LENGTH; h++) {
-            if (packet[h] == 1) {
+
+        for (int h = 0; h < PACKET_LENGTH + CRC_LENGTH; h++) {
+            if (call_packet[h] == 1) {
                 remainder += 1;
             }
         }
@@ -67,7 +74,7 @@ int main() {
         } else {
             errors_detected++;
         }
-        packet[i] = bit; // восстановление исходного бита
+        call_packet[i] = bit; // восстановление исходного бита
     }
 
     // Вывод отчета об ошибках
